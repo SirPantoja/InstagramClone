199fa82d79e758473d16ca398ac1ed0691d8bba3
Andy Pantoja <sirpantoja@fb.com>
2020-07-06 14:29:06 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare variables
+    public static final String TAG = "MainActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Link up views
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Set on click listener for btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Login button clicked");
+                // Call to log in the user
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempt to login user " + username);
+    }
+}
