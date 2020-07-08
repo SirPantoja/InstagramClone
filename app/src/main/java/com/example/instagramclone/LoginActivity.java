d4fa2d98cf8f04d43aa69eeff7a4a7a69981c3b3
Andy Pantoja <sirpantoja@fb.com>
2020-07-08 12:55:13 -0700

    Added signup and icon, imported refresh lib

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,18 +22,24 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
         // Link up views
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
-        // Set on click listener for btnLogin
+        // Set on click listener for btnLogin and btnSignUp
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,10 +50,59 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username, password);
             }
         });
+        
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Sign Up button clicked");
+                // Call to sign up the user
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUpUser(username, password);
+            }
+        });
+        
+    }
+
+    private void signUpUser(final String username, final String password) {
+        Log.i(TAG, "Attempt to sign up user" + username);
+        // Error checking
+        if (username.isEmpty() || password.isEmpty()) {
+            Log.e(TAG, "Empty fields");
+            Toast.makeText(this, "Fields cannot be blank", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Toast.makeText(LoginActivity.this, "Signed up!", Toast.LENGTH_SHORT).show();
+                    loginUser(username, password);
+                    return;
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.e(TAG, "Error with Parse sign in", e);
+                    Toast.makeText(LoginActivity.this, "Username or Password taken", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempt to login user " + username);
+        // Error checking
+        if (username.isEmpty() || password.isEmpty()) {
+            Log.e(TAG, "Empty fields");
+            Toast.makeText(this, "Fields cannot be blank", Toast.LENGTH_SHORT).show();
+            return;
+        }
         // Check the username and password
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
@@ -54,6 +110,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (e != null) {
                     // TODO Better error handling
                     Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 // Request succeeded so go to main activity
